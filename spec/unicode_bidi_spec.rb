a1dfe77eaad5d72c990155fc2607616621e2e83a
Marcin Piotr Wróbel <marcin@MBP14.local>
2024-07-19 11:14:06 +0200

    Added a spec

--- spec/unicode_bidi_spec.rb
+++ spec/unicode_bidi_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe UnicodeBidi do
-  it "has a version number" do
+  it 'has a version number' do
     expect(UnicodeBidi::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'reverses RTL substrings within the string' do
+    expect(UnicodeBidi.bidi('02/08/1894 היה יום לפני 03/08/1894')).to eq '03/08/1894 ינפל םוי היה 02/08/1894'
   end
 end
