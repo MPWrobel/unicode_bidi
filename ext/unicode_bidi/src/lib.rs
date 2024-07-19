5c607820ed788bf74345dfd4f61799aa659c9247
Marcin Piotr Wróbel <marcin@MBP14.local>
2024-07-19 11:13:37 +0200

    Wrapped unicode-bidi crate

--- ext/unicode_bidi/src/lib.rs
+++ ext/unicode_bidi/src/lib.rs
@@ -1,12 +1,22 @@
 use magnus::{function, prelude::*, Error, Ruby};
+use unicode_bidi::BidiInfo;
 
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {subject}!")
+fn bidi(text: String) -> String {
+    let bidi_info = BidiInfo::new(&text, None);
+
+    bidi_info
+        .paragraphs
+        .iter()
+        .map(|para| {
+            let line = para.range.clone();
+            bidi_info.reorder_line(para, line)
+        })
+        .collect()
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("UnicodeBidi")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
+    module.define_singleton_method("bidi", function!(bidi, 1))?;
     Ok(())
 }
